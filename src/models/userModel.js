28696c3c238bcf9a03572b4619d3e49a4c486e4d
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-12-14 16:05:15 +0100

    add createAt in model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,23 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
-
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: Adresse e-mail de l'utilisateur (unique)
+ *         password:
+ *           type: string
+ *           description: Mot de passe de l'utilisateur
+ */
 let userSchema = new Schema ({
     email: {
         type: String,
@@ -10,6 +27,10 @@ let userSchema = new Schema ({
     password: {
         type: String,
         required: true 
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now()
     }
 });
 
