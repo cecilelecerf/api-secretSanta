7dd6bfd5182c3c6908c2bdc628b5a5d9de35e568
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-12-15 15:03:06 +0100

    delete console.log()

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -53,7 +53,6 @@ exports.userLogin= async (req,res)=>{
 exports.listenAllUsers = async(_req, res) =>{
     try{
         const users = await User.find({})
-        console.log(users);
         res.status(200).json({users})
     } catch(error){
         console.log(error);
