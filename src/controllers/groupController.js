61337864453e7a3436e9c1cabe607bed71aaa3b2
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-12-17 17:21:43 +0100

    create update and delete controller

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -22,10 +22,50 @@ exports.listenAllGroups = async(_req, res) =>{
 exports.oneGroup = async(req, res)=>{
     try{
         const group = await Group.findById(req.params.group_id);
+        if(!group){
+            res.status(404).json({message: "Group not found"})
+            res.end()
+        }
         res.status(200).json(group)
     }catch(error){
         console.log(error);
         res.status(500).json({message: "Error server."})
+    }
+}
+
+exports.updateGroup = async(req, res)=>{
+    try{
+        const group = await Group.findById(req.params.group_id);
+        if(!group){
+            res.status(404).json({message: "Group not found"})
+            res.end()
+        }
+        if(group.admin !== req.body.user_id){
+            res.status(403).json({message: "You are not an adminstrator of this group"})
+            res.end()
+        }
+        const updateGroup = await Group.findByIdAndUpdate(req.params.group_id, {name: req.body.name, admin: req.body.admin}, {new : true})
+        res.status(200).json(updateGroup)
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error server."})
+    }
+}
+
+exports.deleteGroup = async(req, res)=>{
+    try{
+        const group = await Group.findById(req.params.group_id);
+        if(!group){
+            res.status(404).json({message: "Group not found"})
+            res.end()
+        }
+        if(group.admin !== req.body.user_id){
+            res.status(403).json({message: "You are not an adminstrator of this group"})
+            res.end()
+        }
+        await Group.findByIdAndDelete(req.params.group_id)       
+    }catch(error){
+
     }
 }
 
